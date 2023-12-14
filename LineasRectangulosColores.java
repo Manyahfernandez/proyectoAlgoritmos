f7b21eb119b30683cef1bb934ec739500dde5653
Manuel F <manyahfernandez@gmail.com>
2023-12-13 20:13:07 -0400

    Se modifico inicio del juego

--- LineasRectangulosColores.java
+++ LineasRectangulosColores.java
@@ -58,7 +58,7 @@ public class LineasRectangulosColores{
 		Descripcion: Inicializa el tablero de juego, con los primeros 3 elementos en el tablero.
 	*/
 	//@(*Precondicion: Haber inicializado el juego con iniciarJuego()*)
-	//@(*Postcondicion: Haber colcado en el tablero 3 elemetos de manera aleatoria*)
+	//@(*Postcondicion: Se colocaron en el tablero 3 elemetos de manera aleatoria*)
 	public static void inicializarTablero(){
 		int k, j, i = 0, siguiente, anterior = 7;
 
@@ -300,7 +300,7 @@ public class LineasRectangulosColores{
 	/*
 		Descripcion: Actualiza y repinta la pantalla grafica del juego, colocando los nuevos elementos en este.
 	*/
-	//@(*Precondicion: Haner agregado o movido algun elemento en el tablero*);
+	//@(*Precondicion: Haber agregado o movido algun elemento en el tablero*);
 	//@(*Postconficion: Tablero de juego actualizado*);
 	public static void actualizarEstadoDelJuego(){
 		int longitud = 351, longitudPedazo = longitud/9, i = 0;
@@ -359,7 +359,7 @@ public class LineasRectangulosColores{
 		Descripcion: Determina si es el final del juego.
 		Retonar : boolean --> que indica si queda alguna casilla vacia en el tablero.
 	*/
-	//@ requires true;
+	//@(*Precondicion: Haber inicializado el juego*);
 	//@ ensures \result == (\forall int i; 0 <= i && i < 9; \forall int j;  0 <= j && j < 9; tablero[i][j] != 7);
 	public static /*@ pure @*/boolean determinarFinaldeJuego(){
 		int i = 0, j = 0;
@@ -387,6 +387,7 @@ public class LineasRectangulosColores{
 	/*----Método que suma puntaje del usuario---*/
 	/*
 		Descripcion: Aumenta el puntaje del usuario.
+			Parametros: cantidad : int --> cantidad de elementos eliminados.
 	*/
 	//@ requires cantidad >= 0;
 	//@ ensures \old(puntaje) > puntaje;
@@ -411,11 +412,19 @@ public class LineasRectangulosColores{
 	/*----Método que modifica los elemetos eliminados del tablero---*/
 	/*
 		Descripcion: Determina si es el final del juego.
+		Parametros: posX : int --> elemento a eliminar o colocar en la matriz una vez validado si existe o no una linea o cuadrado.
 	*/
 	//@ requires 0 <= objeto <= 7;
+	//@(*Postcondicion: Modifica el tablero colocando el elemento en las casillas con -1*);
 	public static void matrizProcesados(int objeto){
 		int i = 0, j = 0;
+		//@ maintaining 0 <= i <= 9;
+		//@ maintaining (\forall int k; 0 <= k < i; (\forall int h; 0 <= h && h < 9; tablero[k][h] != -1));
+		//@ decreases 9-i;
 		while(i < 9){
+			//@ maintaining 0 <= j <= 9;
+			//@ maintaining (\forall int h; 0 <= h && h < j; tablero[i][h] != -1);
+			//@ decreases 9-j;
 			while(j < 9){
 				if(tablero[i][j] == -1){
 					tablero[i][j] = objeto;
@@ -427,6 +436,15 @@ public class LineasRectangulosColores{
 		}
 	}
 
+
+	/*----Método que valida la cantidad de elementos para llamar al metodo sumarPuntaje()----*/
+	/*
+		Descripcion: validad la cantidad de elementos eliminada.
+		Parametros: cantidad : int --> cantidad de elemetos eliminados.
+					objeto : int --> tipo de elemento eliminado.
+	*/
+	//@ requires cantidad >= 0;
+	//@ requires 0 <= objeto <= 6;
 	public static void validarCantidad(int cantidad, int objeto){
 		int i = 0, j = 0;
 		if(cantidad >= 5 || (cantidad == 4 && objeto == 0)){
@@ -440,6 +458,18 @@ public class LineasRectangulosColores{
 		}
 	}
 
+	/*----Funcion que valida el siguiente objeto----*/
+	/*
+		Descripcion: Determina si el siguiente objeto es del mismo tipo.
+		Parametros: posX : int --> posicion de la fila en la que se encuentra el elemento.
+					posY : int --> posicion de la columna en la que se encuentra el elemento.
+					objeto : int --> tipo de elemento a validar
+		Retonar : boolean --> que indica si el siguiente elemento es del mismo tipo.
+	*/
+	//@ requires 0 <= posX < 9;
+	//@ requires 0 <= posY < 9;
+	//@ requires 0 <= objeto <= 6;
+	//@ ensures \result == (posX >= 0 && posX < 9 && posY >= 0 && posY < 9 && tablero[posX][posY] == objeto);
 	public static /*@ pure @*/ boolean validarSiquienteObjeto(int posX, int posY, int objeto){
 		if(posX >= 0 && posX < 9 && posY >= 0 && posY < 9 && tablero[posX][posY] == objeto){
 			return true;
@@ -448,6 +478,14 @@ public class LineasRectangulosColores{
 		}
 	}
 
+	/*----Metodo que verifica si se formo alguna linea hacia la derecha----*/
+	/*
+		Descripcion: Determina si se formo una linea hacia la derecha.
+		Parametros: posX : int --> posicion de la fila en la que se encuentra el elemento
+					posY : int --> posicion de la columna en la que se encuentra el elemento
+	*/
+	//@ requires 0 <= posX < 9;
+	//@ requires 0 <= posY < 9;
 	public static void validarLineaDerecha(int posX, int posY){
 		int cantidad  = 1, posicionY= posY;
 		posicionY = posY + 1;
@@ -460,6 +498,12 @@ public class LineasRectangulosColores{
 		validarCantidad(cantidad, tablero[posX][posY]);
 	}
 
+	/*----Metodo que verifica si se formo alguna linea hacia abajo---*/
+	/*
+		Descripcion: Determina si se formo una linea hacia abajo.
+	*/
+	//@ requires 0 <= posX < 9;
+	//@ requires 0 <= posY < 9;
 	public static void validarLineaAbajo(int posX, int posY){
 		int cantidad  = 1, posicionX= posX;
 		posicionX = posX + 1;
@@ -472,6 +516,14 @@ public class LineasRectangulosColores{
 		validarCantidad(cantidad, tablero[posX][posY]);
 	}
 
+	/*----Metodo que verifica si se formo alguna linea diagonal---*/
+	/*
+		Descripcion: Determina si se formo una linea diagonal.
+		Parametros: posX : int --> posicion de la fila en la que se encuentra el elemento
+					posY : int --> posicion de la columna en la que se encuentra el elemento
+	*/
+	//@ requires 0 <= posX < 9;
+	//@ requires 0 <= posY < 9;
 	public static void validarLineaDiagonal(int posX, int posY){
 		int cantidad  = 1, posicionX = posX, posicionY = posY;
 		posicionX = posX + 1;
@@ -486,6 +538,14 @@ public class LineasRectangulosColores{
 		validarCantidad(cantidad, tablero[posX][posY]);
 	}
 
+	/*----Metodo que verifica si se formo alguna diagonal contraria----*/
+	/*
+		Descripcion: Determina si se formo una linea diagonal contraria
+		Parametros: posX : int --> posicion de la fila en la que se encuentra el elemento
+					posY : int --> posicion de la columna en la que se encuentra el elemento
+	*/
+	//@ requires 0 <= posX < 9;
+	//@ requires 0 <= posY < 9;
 	public static void validarLineaDiagonalContraria(int posX, int posY){
 		int cantidad  = 1, posicionX = posX, posicionY = posY;
 		posicionX = posX - 1;
@@ -500,6 +560,14 @@ public class LineasRectangulosColores{
 		validarCantidad(cantidad, tablero[posX][posY]);
 	}
 
+	/*----Metodo que verifica si existe un cuadrado---*/
+	/*
+		Descripcion: Determina si se formo un cuadrado
+		Parametros: posX : int --> posicion de la fila en la que se encuentra el elemento
+					posY : int --> posicion de la columna en la que se encuentra el elemento
+	*/
+	//@ requires 0 <= posX < 9;
+	//@ requires 0 <= posY < 9;
 	public static /*@ pure @*/ int validarCuadrado(int posX, int posY){
 		int posicionY = posY, i = posX, j = posY, cantidady = 1, cantidad, cantidadx = 1, cantidadRetornada = 0, posicionX;
 		posicionX = posX + 1;
@@ -542,12 +610,18 @@ public class LineasRectangulosColores{
 		return cantidadRetornada;
 	}
 
+	/*----Metodo que llama a la funcion validarCuadrado()----*/
+	/*
+		Descripcion: Determina si se formo una linea haca la derecha.
+		Parametros: posX : int --> posicion de la fila en la que se encuentra el elemento
+					posY : int --> posicion de la columna en la que se encuentra el elemento
+	*/
+	//@ requires 0 <= posX < 9;
+	//@ requires 0 <= posY < 9;
 	public static void cuadradoVerificar(int posX, int posY){
 		int cantidad = validarCuadrado(posX, posY);
 		if(cantidad != -1){
 			validarCantidad(cantidad,0);
-			System.out.println("Elimine algo");
-			System.out.println(cantidad);
 			seElimino = true;
 		}else{
 			matrizProcesados(0);
@@ -592,7 +666,30 @@ public class LineasRectangulosColores{
 		return false;
 	}
 
-	public static void menu(){
+	public static int seleccion;
+
+	public static void iniciarElJuego(){
+		LineasRectangulosColores.inicializarJuego();
+		LineasRectangulosColores.inicializarTablero();
+		LineasRectangulosColores.obtenerProximosObjetos();
+		LineasRectangulosColores.mostrarEstadoDelJuego();
+
+		while(!LineasRectangulosColores.determinarFinaldeJuego()){
+			LineasRectangulosColores.obtenerJugadasValida();
+			LineasRectangulosColores.actualizarEstadoDelJuego();
+			if(!LineasRectangulosColores.procesarObjetosDelTablero()){
+				LineasRectangulosColores.agregarProximosObjetos();
+				LineasRectangulosColores.obtenerProximosObjetos();
+			}
+			LineasRectangulosColores.actualizarEstadoDelJuego();
+		}
+
+		LineasRectangulosColores.procesarFinalJuego();
+		System.out.println("\t Juego Terminado");
+		System.out.println("Puntaje: " + puntaje);
+	}
+
+	public static void instruccionesGrafico(){
 		mt = new MaquinaDeTrazados(450, 450, "Lineas de colores y rectangulos", Colores.WHITE);
 		mt.dibujarRectanguloLleno(0,0,15,15,COLORESSELECCIONADOS[0]);
 		mt.dibujarRectangulo(0,0,15,15);
@@ -609,7 +706,7 @@ public class LineasRectangulosColores{
 		mt.dibujarOvaloLleno(90,0,15,15,COLORESSELECCIONADOS[6]);
 		mt.dibujarOvalo(90,0,15,15);
 		mt.configurarFuente("SansSerif", Font.BOLD, 12);
-		mt.dibujarString("Lineas y Rectangulos de Colores",110, 15, Colores.BLACK);
+		mt.dibujarString("Instrucciones del juego",150, 15, Colores.BLACK);
 		mt.dibujarOvaloLleno(345,0,15,15,COLORESSELECCIONADOS[6]);
 		mt.dibujarOvalo(345,0,15,15);
 		mt.dibujarOvaloLleno(360,0,15,15,COLORESSELECCIONADOS[5]);
@@ -624,6 +721,33 @@ public class LineasRectangulosColores{
 		mt.dibujarOvalo(420,0,15,15);
 		mt.dibujarRectanguloLleno(435,0,15,15,COLORESSELECCIONADOS[0]);
 		mt.dibujarRectangulo(435,0,15,15);
+
+		mt.configurarFuente("SansSerif", Font.BOLD, 12);
+		mt.dibujarString("En el juego 'Lineas de colores y rectangulos' el jugador",5, 45, Colores.BLACK);
+		mt.dibujarString("debera formar lineas de esferas o formar cuadrados, con las",0, 57, Colores.BLACK);
+		mt.dibujarString("siguientes instrucciones: ",0, 69, Colores.BLACK);
+
+		mt.dibujarString("1. Formar lineas de 5 o mas esferas del mismo color ya sean",5, 93, Colores.BLACK);
+		mt.dibujarString("hacia arriba, abajo,hacia un lado o diagonal.",0, 105, Colores.BLACK);
+		mt.dibujarString("2. Formar cuadrados de 3 x 3 o mas.",5, 117, Colores.BLACK);
+		mt.dibujarString("La consola podra arrojar alguno de los siguientes mensajes: ",5, 141, Colores.BLACK);
+		mt.dibujarString("al ingresar un movimiento",0, 153, Colores.BLACK);
+		mt.dibujarString("1.'Elemento movido a la casilla: ' si el elemento seleccionado",5, 177, Colores.BLACK);
+		mt.dibujarString("por el usuario se movio a la casilla de manera exitosa",0, 189, Colores.BLACK);
+		mt.dibujarString("2. 'Elemento seleccionado no puede ser movido' si el elemento",5, 201, Colores.BLACK);
+		mt.dibujarString("no posee a su alrededor alguna casilla vacia.",0, 213, Colores.BLACK);
+		mt.dibujarString("3. 'Casilla [i] [j] no se encuentra vacia' si la casilla [i][j] a la que",5, 225, Colores.BLACK);
+		mt.dibujarString("se quiere mover un elemento no se encuentra vacia.",0, 237, Colores.BLACK);
+		mt.dibujarString("4. 'La casilla [i] [j] se encuentra vacia' si la casilla de la cual",5, 249, Colores.BLACK);
+		mt.dibujarString("se quiere mover un elemento se encuentra vacia.",0, 261, Colores.BLACK);
+		mt.dibujarString("5. 'Movimiento invalido, posicion se sale del tablero' si alguna",5, 273, Colores.BLACK);
+		mt.dibujarString("fila o columna es mayor a los valores permitidos",0, 285, Colores.BLACK);
+
+		mt.dibujarString("El usuario primero ingresara la fila y la columna del elemento",5,309, Colores.BLACK);
+		mt.dibujarString("a mover y despues la fila y columna de la casilla a la cual se",0, 321, Colores.BLACK);
+		mt.dibujarString("desea mover el elemento, a la derecha se muestra el numero",0, 333, Colores.BLACK);
+		mt.dibujarString("de cada fila y abajo el numero de cada columna",0, 345, Colores.BLACK);
+		mt.dibujarString("DIVIERTETE...!!! PARA INICIAR EL JUEGO INGRESA 1",50, 381, Colores.MAGENTA);
 		mt.mostrar();
 	}
 
@@ -638,9 +762,7 @@ public class LineasRectangulosColores{
 	}
 
 	public static void main(String[] args) {
-		int seleccion;
 		Scanner usuario = new Scanner(System.in);
-		//LineasRectangulosColores.menu();
 		System.out.println("\t Lineas de Rectangulos y Colores: ");
 		System.out.println("1. Iniciar Juego");
 		System.out.println("2. Ver instrucciones del juego");
@@ -653,28 +775,25 @@ public class LineasRectangulosColores{
 				seleccion = -1;
 			}
 		}while(seleccion == -1);
-
+		
 		if(seleccion == 1){
-			//mt.terminar();
-			LineasRectangulosColores.inicializarJuego();
-			LineasRectangulosColores.inicializarTablero();
-			LineasRectangulosColores.obtenerProximosObjetos();
-			LineasRectangulosColores.mostrarEstadoDelJuego();
-
-			while(!LineasRectangulosColores.determinarFinaldeJuego()){
-				LineasRectangulosColores.obtenerJugadasValida();
-				LineasRectangulosColores.actualizarEstadoDelJuego();
-				if(!LineasRectangulosColores.procesarObjetosDelTablero()){
-					LineasRectangulosColores.agregarProximosObjetos();
-					LineasRectangulosColores.obtenerProximosObjetos();
+			LineasRectangulosColores.iniciarElJuego();
+		}
+		if(seleccion == 2){
+			LineasRectangulosColores.instruccionesGrafico();
+			System.out.println("Ingresa 1 para comenzar el juego: ");
+			do{
+				seleccion = usuario.nextInt();
+				if(seleccion != 1){
+					System.out.println("Opcion invalida. Ingresa 1 para comenzar el juego: ");
+					seleccion = -1;
 				}
-				LineasRectangulosColores.actualizarEstadoDelJuego();
-			}
-
-			LineasRectangulosColores.procesarFinalJuego();
+			}while(seleccion == -1);
+			mt.terminar();
+			LineasRectangulosColores.iniciarElJuego();
 		}
 		if(seleccion == 3){
-			mt.terminar();
+			System.exit(0);
 		}
 	}
 }
--- Probar.java
+++ Probar.java
@@ -1,17 +1,90 @@
 import java.awt.Font;
 import java.util.Scanner;
 public class Probar{
-	public static void main(String[] args) {
-		MaquinaDeTrazados mt = new MaquinaDeTrazados(500, 500, "Lineas de colores y rectangulos", Colores.WHITE);
-		mt.dibujarRectanguloLleno(0,150,500,100,Colores.WHITE);
-		mt.dibujarRectangulo(0,150,500,100);
-		mt.configurarFuente("Monospaced", Font.BOLD, 24);
-		mt.dibujarString("Juego Terminado",150,210, Colores.BLACK);
+	public static final Colores[] COLORESSELECCIONADOS = {Colores.MAGENTA, Colores.BLUE, Colores.CYAN, Colores.GREEN, Colores.RED, Colores.YELLOW, Colores.WHITE};
 
-		mt.configurarFuente("Monospaced", Font.BOLD, 21);
-		mt.dibujarRectanguloLleno(0, 260, 500, 50, Colores.BLACK);
-		mt.dibujarRectangulo(0, 260, 500, 50, Colores.ORANGE);
-		mt.dibujarString("Puntaje",185,295, Colores.WHITE);
+	public static void main(String[] args) {
+
+		MaquinaDeTrazados mt = new MaquinaDeTrazados(450, 450, "Lineas de colores y rectangulos", Colores.WHITE);
+		mt.dibujarRectanguloLleno(0,0,15,15,COLORESSELECCIONADOS[0]);
+		mt.dibujarRectangulo(0,0,15,15);
+		mt.dibujarOvaloLleno(15,0,15,15,COLORESSELECCIONADOS[1]);
+		mt.dibujarOvalo(15,0,15,15);
+		mt.dibujarOvaloLleno(30,0,15,15,COLORESSELECCIONADOS[2]);
+		mt.dibujarOvalo(30,0,15,15);
+		mt.dibujarOvaloLleno(45,0,15,15,COLORESSELECCIONADOS[3]);
+		mt.dibujarOvalo(45,0,15,15);
+		mt.dibujarOvaloLleno(60,0,15,15,COLORESSELECCIONADOS[4]);
+		mt.dibujarOvalo(60,0,15,15);
+		mt.dibujarOvaloLleno(75,0,15,15,COLORESSELECCIONADOS[5]);
+		mt.dibujarOvalo(75,0,15,15);
+		mt.dibujarOvaloLleno(90,0,15,15,COLORESSELECCIONADOS[6]);
+		mt.dibujarOvalo(90,0,15,15);
+		mt.configurarFuente("SansSerif", Font.BOLD, 12);
+		mt.dibujarString("Instrucciones del juego",150, 15, Colores.BLACK);
+		mt.dibujarOvaloLleno(345,0,15,15,COLORESSELECCIONADOS[6]);
+		mt.dibujarOvalo(345,0,15,15);
+		mt.dibujarOvaloLleno(360,0,15,15,COLORESSELECCIONADOS[5]);
+		mt.dibujarOvalo(360,0,15,15);
+		mt.dibujarOvaloLleno(375,0,15,15,COLORESSELECCIONADOS[4]);
+		mt.dibujarOvalo(375,0,15,15);
+		mt.dibujarOvaloLleno(390,0,15,15,COLORESSELECCIONADOS[3]);
+		mt.dibujarOvalo(390,0,15,15);
+		mt.dibujarOvaloLleno(405,0,15,15,COLORESSELECCIONADOS[2]);
+		mt.dibujarOvalo(405,0,15,15);
+		mt.dibujarOvaloLleno(420,0,15,15,COLORESSELECCIONADOS[1]);
+		mt.dibujarOvalo(420,0,15,15);
+		mt.dibujarRectanguloLleno(435,0,15,15,COLORESSELECCIONADOS[0]);
+		mt.dibujarRectangulo(435,0,15,15);
+
+		mt.configurarFuente("SansSerif", Font.BOLD, 12);
+		mt.dibujarString("En el juego 'Lineas de colores y rectangulos' el jugador",5, 45, Colores.BLACK);
+		mt.dibujarString("debera formar lineas de esferas o formar cuadrados, con las",0, 57, Colores.BLACK);
+		mt.dibujarString("siguientes instrucciones: ",0, 69, Colores.BLACK);
+
+		mt.dibujarString("1. Formar lineas de 5 o mas esferas del mismo color ya sean",5, 93, Colores.BLACK);
+		mt.dibujarString("hacia arriba, abajo o diagonal.",0, 105, Colores.BLACK);
+		mt.dibujarString("2. Formar cuadrados de 3 x 3 o mas.",5, 117, Colores.BLACK);
+		mt.dibujarString("La consola podra arrojar alguno de los siguientes mensajes: ",5, 141, Colores.BLACK);
+		mt.dibujarString("al ingresar un movimiento",0, 153, Colores.BLACK);
+		mt.dibujarString("1.'Elemento movido a la casilla: ' si el elemento seleccionado",5, 177, Colores.BLACK);
+		mt.dibujarString("por el usuario se movio a la casilla de manera exitosa",0, 189, Colores.BLACK);
+		mt.dibujarString("2. 'Elemento seleccionado no puede ser movido' si el elemento",5, 201, Colores.BLACK);
+		mt.dibujarString("no posee a su alrededor alguna casilla vacia.",0, 213, Colores.BLACK);
+		mt.dibujarString("3. 'Casilla [i] [j] no se encuentra vacia' si la casilla [i][j] a la que",5, 225, Colores.BLACK);
+		mt.dibujarString("se quiere mover un elemento no se encuentra vacia.",0, 237, Colores.BLACK);
+		mt.dibujarString("4. 'La casilla [i] [j] se encuentra vacia' si la casilla de la cual",5, 249, Colores.BLACK);
+		mt.dibujarString("se quiere mover un elemento se encuentra vacia.",0, 261, Colores.BLACK);
+		mt.dibujarString("5. 'Movimiento invalido, posicion se sale del tablero' si alguna",5, 273, Colores.BLACK);
+		mt.dibujarString("fila o columna es mayor a los valores permitidos",0, 285, Colores.BLACK);
+
+		mt.dibujarString("El usuario primero ingresara la fila y la columna del elemento",5,309, Colores.BLACK);
+		mt.dibujarString("a mover y despues la fila y columna de la casilla a la cual se",0, 321, Colores.BLACK);
+		mt.dibujarString("desea mover elemento, a la derecha se muestra el numero de cada",0, 333, Colores.BLACK);
+		mt.dibujarString("fila y abajo el numero de cada columna",0, 345, Colores.BLACK);
+		mt.dibujarString("DIVIERTETE",0, 357, Colores.MAGENTA);
+		mt.configurarFuente("SansSerif", Font.BOLD, 12);
+		mt.dibujarString("Para regresar al menu anterior ingresa el valor 0",0, 369, Colores.BLACK);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		 
+
+
 		mt.mostrar();
 	}
 }
